43f5535c4d5eeae53afe63c3a314018003a3109d
Your Name <you@example.com>
2020-01-29 13:22:30 -0500

    Calls template generator script and removed gui option

--- CMD_Euler/cmd_euler.sh
+++ CMD_Euler/cmd_euler.sh
@@ -7,25 +7,21 @@
 cmd_euler()
 (
 	local WRK_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-	local LAUNCH_GUI=0
 	local PRINT_HELP=0
 	local PROBLEM_NUMBER=0
 	local GEN_TEMPLATE=0
 	local TEMPLATE=""
 
 	usage() {
-		echo "USAGE: cmd_euler [-g] [-p <problem-number>] [-t <language-template>]"
+		echo "USAGE: cmd_euler [-p <problem-number>] [-t <language-template>]"
 	}
 
 	# Given an argument string, will parse arguments
 	# Populates global variables with
 	# Argument specified user-variables
 	parse_args() {
-		while getopts ":ghp:t:" o; do
+		while getopts ":hp:t:" o; do
 			case "${o}" in
-			g)
-				LAUNCH_GUI=1
-				;;
 			h)
 				PRINT_HELP=1
 				;;
@@ -53,51 +49,45 @@ cmd_euler()
 		shift $((OPTIND-1))
 	}
 
-	call_feature() {
-		if [ $LAUNCH_GUI -ne 0 ]
+	validate_args() {
+		re='^[0-9]+$'
+		if ! [[ $PROBLEM_NUMBER =~ $re ]] || [ $PROBLEM_NUMBER -lt 1 ] || [ $PROBLEM_NUMBER -gt 699 ]
 		then
-			echo "GUI"
+			echo "ERROR: No valid problem number specified!" >&2
+			usage
+			return 1
 		fi
+	}
 
+	call_feature() {
 		if [ $PRINT_HELP -ne 0 ]
 		then
 			usage
-		fi
-
-		if [ ${#TEMPLATE} -ne 0 ] && [ $PROBLEM_NUMBER -ne 0 ]
+			return 0
+		elif [ ${#TEMPLATE} -ne 0 ] && [ $PROBLEM_NUMBER -ne 0 ]
 		then
-			for t in $(ls ${WRK_DIR}/templates)
-			do
-				if [[ ${t,,} == ${TEMPLATE,,} ]]
-				then
-					cat ${WRK_DIR}/templates/$t/template.*
-				fi
-			done
-
-			echo "GENERATING $TEMPLATE TEMPLATE FOR PROBLEM $PROBLEM_NUMBER"
-			bash ${WRK_DIR}/scripts/get_euler.sh ${PROBLEM_NUMBER}
+			bash ${WRK_DIR}/scripts/make_template.sh "${TEMPLATE}" ${PROBLEM_NUMBER}
 		elif [ $PROBLEM_NUMBER -ne 0 ]
 		then
 			echo "GET PROBLEM $PROBLEM_NUMBER"
 			bash ${WRK_DIR}/scripts/get_euler.sh ${PROBLEM_NUMBER}
-		fi
-
-		if [ ${#TEMPLATE} -eq $PROBLEM_NUMBER ] && [ $PRINT_HELP -eq $LAUNCH_GUI ]
+		elif [ ${#TEMPLATE} -eq $PROBLEM_NUMBER ] && [ $PRINT_HELP -eq $LAUNCH_GUI ]
 		then
 			usage
+			return 1
 		fi
-
 	}
 
 	# Parses user-arguments
 	# Call aprropriate feature depending upon user demand
 	main() {
-		parse_args $@
-		call_feature
+		parse_args $@ || return 1
+		validate_args || return 1
+		call_feature || return 1
 	}
 
 	## MAIN FUNCTION CALL ##
-	main $@
+	main $@ || return 1
 )
 
 cmd_euler $@
