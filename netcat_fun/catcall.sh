08ed566a3c01d418736cecb0358c583b35217dcf
Your Name <you@example.com>
2020-04-05 03:37:33 -0400

    localized function vars, added advertise port to catcall_host, added while loop to catcall_connect

--- netcat_fun/catcall.sh
+++ netcat_fun/catcall.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
-# 1. Run catcall_connect <IP> <PORT> in a while loop on CLIENT
-# 2. Run catcall_host <PORT> on server
-catcall_host() {
-	PORT=${1}
-	arecord 2>/dev/null | nc -l ${PORT} | aplay &>/dev/null &
+# 1. Run catcall_connect <IP> <PORT> on CLIENT
+# 2. Run catcall_host <PORT> on SERVER
+
+# Server will advertise on PORT
+# Serve audio on PORT+1
+# Serve video on PORT+2
+catcall_host() (
+	local PORT=${1}
+	nc -l ${PORT} &
+	let PORT++
+	arecord 2>/dev/null | nc -l ${PORT} 2>/dev/null | aplay &>/dev/null &
 	let PORT++
 	ffmpeg -f video4linux2  -i /dev/video0 -vcodec libx264 -b 1000k -f matroska -y /dev/stdout 2>/dev/null | nc -l ${PORT} | mplayer - &>/dev/null
-}
+)
 
+# When server advertise PORT is up, connect audio and video
 catcall_connect() {
-	IP_ADDR=${1}
-	PORT=${2}
-	arecord 2>/dev/null | nc ${IP_ADDR} ${PORT} | aplay &>/dev/null &
+	local IP_ADDR=${1}
+	local PORT=${2}
+	local TRY=0
+	printf "\nAwaiting ${IP_ADDR}:${PORT} to advertise connection:\n"
+	while ! nc -dzW 1 ${IP_ADDR} ${PORT} &>/dev/null
+	do
+		[ $TRY -eq 0 ] && printf '.'
+		TRY=$(((TRY + 1) % 100))
+	done
+	printf '\nCONNECTION ACHIEVED!\n'
+	let PORT++
+	arecord 2>/dev/null | nc ${IP_ADDR} ${PORT} 2>/dev/null | aplay &>/dev/null &
 	let PORT++
 	ffmpeg -f video4linux2  -i /dev/video0 -vcodec libx264 -b 1000k -f matroska -y /dev/stdout 2>/dev/null | nc ${IP_ADDR} ${PORT} | stdbuf -i0 mplayer - &>/dev/null
 }
