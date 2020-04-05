9cf657dc12b0058e3b75f439a6ccf2406b34b0ee
Your Name <you@example.com>
2020-04-05 02:35:56 -0400

    merged catcatll and meowcam into single netcat-skype like program called catcall

--- netcat_fun/catcall.sh
+++ netcat_fun/catcall.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-# Best when catcall_connect is run FIRST in a while loop on a client machine
-# SECOND catcatll_host should be run on a server machine
+# 1. Run catcall_connect <IP> <PORT> in a while loop on CLIENT
+# 2. Run catcall_host <PORT> on server
 catcall_host() {
 	PORT=${1}
-	arecord | nc -l ${PORT} | aplay 2>/dev/null
+	arecord 2>/dev/null | nc -l ${PORT} | aplay &>/dev/null &
+	let PORT++
+	ffmpeg -f video4linux2  -i /dev/video0 -vcodec libx264 -b 1000k -f matroska -y /dev/stdout 2>/dev/null | nc -l ${PORT} | mplayer - &>/dev/null
 }
 
 catcall_connect() {
 	IP_ADDR=${1}
 	PORT=${2}
-	arecord | nc ${IP_ADDR} ${PORT} | aplay 2>/dev/null
+	arecord 2>/dev/null | nc ${IP_ADDR} ${PORT} | aplay &>/dev/null &
+	let PORT++
+	ffmpeg -f video4linux2  -i /dev/video0 -vcodec libx264 -b 1000k -f matroska -y /dev/stdout 2>/dev/null | nc ${IP_ADDR} ${PORT} | stdbuf -i0 mplayer - &>/dev/null
 }
