0b44f6342aa6272125d91cfe3a92658dbcef5653
Your Name <you@example.com>
2020-04-07 16:07:25 -0400

    Added special case for box drawing chars

--- webTools/symbol.sh
+++ webTools/symbol.sh
@@ -3,11 +3,19 @@
 symbol() {
 	SYMBOL="${@}"
 	SYMBOL_FILE=/tmp/symbol_file.html
-	curl 'https://en.wikipedia.org/wiki/List_of_Unicode_characters' 2>/dev/null > ${SYMBOL_FILE}
+	# Special case for box chars
+	if [[ ${SYMBOL^^} == "BOX" ]]
+	then
+		curl 'https://en.wikipedia.org/wiki/Box-drawing_character' 2>/dev/null --output ${SYMBOL_FILE}
+		grep '<td>.*</td>' ${SYMBOL_FILE} | sed -r 's/<td>//g; s#</td>##g; /^\s*$/d' | awk '!a[$0]++'
+		echo "══════════════════════════════════════════════"
+	fi
+	# Standard unicode
+	curl 'https://en.wikipedia.org/wiki/List_of_Unicode_characters' 2>/dev/null --output ${SYMBOL_FILE}
 	cat ${SYMBOL_FILE} | sed 's#</td>##g' | sed -r '/^\s*$/d; s#<td align="center">#<td>#g' | grep -e '<td>' | grep -B 10 -i "$SYMBOL" | sed '/^.\{6\}./d' | sed 's#<td>##g; s#--##g' | sed -r 's/[0-9]{1,10}$//' | sed -r '/^\s*$/d'
 	cat ${SYMBOL_FILE} | grep 'class="mw-redirect" title="' | grep -i "${SYMBOL}" | tr '"' '\n' | sed '/^.\{1\}./d' | sed -r 's/[0-9]{1,10}$//' | sed -r '/^\s*$/d'
-
-	curl 'https://en.wikipedia.org/wiki/Dingbat' 2>/dev/null > ${SYMBOL_FILE}
+	# Dingbats
+	curl 'https://en.wikipedia.org/wiki/Dingbat' 2>/dev/null --output ${SYMBOL_FILE}
 	cat ${SYMBOL_FILE} | sed 's#</td>##g' | sed -r '/^\s*$/d; s#<td align="center">#<td>#g' | grep -e '<td>' | grep -B 10 -i "$SYMBOL" | sed '/^.\{6\}./d' | sed 's#<td>##g; s#--##g' | sed -r 's/[0-9]{1,10}$//' | sed -r '/^\s*$/d'
 	cat ${SYMBOL_FILE} | grep 'class="mw-redirect" title="' | grep -i "${SYMBOL}" | tr '"' '\n' | sed '/^.\{1\}./d' | sed -r 's/[0-9]{1,10}$//' | sed -r '/^\s*$/d'
 }
