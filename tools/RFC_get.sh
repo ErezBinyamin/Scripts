732f492d2206129a76a2738af15ea82a84db26eb
Your Name <you@example.com>
2020-04-18 16:19:13 -0400

    :list and MAX_RFC get info from https://www.ietf.org/download/rfc-index.txt

--- tools/RFC_get.sh
+++ tools/RFC_get.sh
@@ -4,9 +4,14 @@ RFC_get()
 {
 	mkdir -p /tmp/RFC_get
 	local WEB_RESP="/tmp/RFC_get/rfc_get_web_resp_${RANDOM}.html"
-	local MIN_RFC=1
-	local MAX_RFC=8650
+	local RFC_INDEX="/tmp/RFC_get/rfc_index_${RANDOM}.html"
 	local isNum='^[0-9]+$'
+
+	# Update RFC_INDEX
+	curl 'https://www.ietf.org/download/rfc-index.txt' 2>/dev/null > ${RFC_INDEX}
+
+	local MIN_RFC=1
+	local MAX_RFC=$(sed '/^ / d' ${RFC_INDEX} | tail -n 1 | sed 's/ .*//')
 	local NEVER_ISSUED=( "3333" "3350" "3399" "3699" "3799" "3800" "3899" "3900" "3907" "3908" "3999" "4000" "4099" "4100" "4199" "4200" "4232" "4299" "4300" "4399" "4400" "4499" "4500" "4599" "4600" "4658" "4699" "4700" "4751" "4799" "4800" "4899" "4900" "4921" "4922" "4989" "4999" "5099" "5100" "5108" "5199" "5200" "5299" "5300" "5312" "5313" "5314" "5315" "5319" "5399" "5400" "5499" "5500" "5599" "5600" "5699" "5700" "5799" "5800" "5809" "5821" "5822" "5823" "5899" "5900" "5999" "6000" "6099" "6100" "6102" "6103" "6199" "6200" "6299" "6300" "6399" "6400" "6499" "6500" "6523" "6524" "6599" "6600" "6634" "6699" "6700" "6799" "6800" "6899" "6900" "6966" "6995" "6999" "7000" "7099" "7327" "7907" "8523" "8524" "8535" "8566" "8626" "8644" "8646" "8647" "8648" )
 
 	# Syntax check Usage statement
@@ -67,11 +72,9 @@ RFC_get()
 	# Print list of available RFCs
 	elif [[ "${1,,}" == ":list" ]]
 	then
-		echo "Valid RFC numbers: [ ${MIN_RFC} - ${MAX_RFC} ]"
-		#for i in `seq 1 $MAX_RFC`
-		#do
-		#	echo "${NEVER_ISSUED[@]}" | tr ' ' '\n' | grep -qFx $i || echo $i
-		#done
+		#curl 'https://www.ietf.org/download/rfc-index.txt' 2>/dev/null > ${RFC_INDEX}
+		cat ${RFC_INDEX}
+		printf "\nValid RFC numbers: [ ${MIN_RFC} - ${MAX_RFC} ]\n"
 		return 0
 	# Print list of RFCs related to keyword:   RFC_N	RFC_Title
 	else
