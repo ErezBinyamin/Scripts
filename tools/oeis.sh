53174eac08b0a9213c8890f29c03cd79374419c1
Your Name <you@example.com>
2020-05-06 16:17:04 -0400

    first functional version

--- tools/oeis.sh
+++ tools/oeis.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
 # OEIS is the Online Encyclopedia of Integer Sequences
 # oeis.sh is the command line way to access it :)
-
-BASE_URL='https://oeis.org/'
-oeis() (
-	local URL='https://oeis.org/search?q='
+oeis() {
+	local URL='https://oeis.org'
 	local TMP=/tmp/oeis
 	local DOC=/tmp/oeis/doc.html
 	mkdir -p $TMP
 
-	# Build URL
-	URL+="$(echo $@ | tr ' ' ',')"
+	# Search sequence by ID
+	if [ $# -eq 1 ]
+	then
+		[[ ${1:0:1} == 'A' ]] && ID="A$(printf '%06d' ${1:1})" || ID="A$(printf '%06d' ${1})"
+		URL+="/${ID}"
+		curl $URL 2>/dev/null > $DOC
+		printf "ID: ${ID}\n"
+		grep -A 1 '<td valign=top align=left>' $DOC | sed '/<td valign=top align=left>/d; /--/d; s/^[ \t]*//'
+		printf "\n"
+		grep -o '<tt>[0-9].*, [0-9].*[0-9]</tt>' $DOC | sed 's/<[^>]*>//g' | grep -v '[a-z]'
+		printf "\n"
+		cat $DOC | tr '\n' '@' | grep -o 'PROG.*CROSSREFS' | tr '@' '\n' | sed 's/^[ \t]*//; s/<[^>]*>//g; s/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g; /^\s*$/d; /CROSSREFS/d; /PROG/d'
+		printf "\n"
+	# Search unknown sequence
+	else
+		# Build URL
+		URL+="/search?q=$(echo $@ | tr ' ' ',')"
+		# Retrieve DOC
+		curl $URL 2>/dev/null > $DOC
 
-	# Retrieve DOC
-	curl $URL 2>/dev/null > $DOC
+		grep -o '=id:.*&' $DOC | sed 's/=id://; s/&//' > $TMP/id
+		grep -A 1 '<td valign=top align=left>' $DOC | sed '/<td valign=top align=left>/d; /--/d; s/^[ \t]*//' > $TMP/nam
+		grep -o '<tt>.*<b.*</tt>' $DOC | sed 's/<[^>]*>//g' > $TMP/seq
 
-	# Get sequences names and values
-	[ $# -ne 1 ] && grep -o '=id:.*&' $DOC | sed 's/=id://; s/&//' > $TMP/nam
-	[ $# -ne 1 ] && grep -o '<tt>.*<b.*</tt>' $DOC | sed 's/<[^>]*>//g' > $TMP/seq
-
-	[ $# -eq 1 ] && cat $DOC # > $TMP/nam
-	#[ $# -eq 1 ] && cat $DOC # > $TMP/seq
-
-	# Print data
-	readarray -t NAM < $TMP/nam
-	readarray -t SEQ < $TMP/seq
-	for i in ${!NAM[@]}
-	do
-		echo ${NAM[$i]}
-		echo ${SEQ[$i]}
-	done
-)
+		# Print data
+		readarray -t ID < $TMP/id
+		readarray -t NAM < $TMP/nam
+		readarray -t SEQ < $TMP/seq
+		for i in ${!NAM[@]}
+		do
+			printf "${ID[$i]}: ${NAM[$i]}\n"
+			echo ${SEQ[$i]}
+			echo "--"
+		done
+	fi
+}
 
 oeis $@
