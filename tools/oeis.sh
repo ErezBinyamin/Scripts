5e475435d84def417eaf36f94a0e074ec1ba8b88
Your Name <you@example.com>
2020-05-06 17:12:54 -0400

    chubin styles

--- tools/oeis.sh
+++ tools/oeis.sh
@@ -1,50 +1,69 @@
-#!/bin/bash
-# OEIS is the Online Encyclopedia of Integer Sequences
-# oeis.sh is the command line way to access it :)
+#!/usr/bin/env bash
+
+# Written by Erez Binyamin (github.com/ErezBinyamin)
+
+# Search for an integer sequence
 oeis() {
-	local URL='https://oeis.org'
-	local TMP=/tmp/oeis
-	local DOC=/tmp/oeis/doc.html
-	mkdir -p $TMP
-
-	# Search sequence by ID
-	if [ $# -eq 1 ]
-	then
-		[[ ${1:0:1} == 'A' ]] && ID=${1:1} || ID=${1}
-		ID=$(bc <<< "$ID")
-		ID="A$(printf '%06d' ${ID})"
-
-		URL+="/${ID}"
-		curl $URL 2>/dev/null > $DOC
-		printf "ID: ${ID}\n"
-		grep -A 1 '<td valign=top align=left>' $DOC | sed '/<td valign=top align=left>/d; /--/d; s/^[ \t]*//; s/<[^>]*>//g;'
-		printf "\n"
-		grep -o '<tt>.*, .*[0-9]</tt>' $DOC | sed 's/<[^>]*>//g' | grep -v '[a-z]'
-		printf "\n"
-		cat $DOC | tr '\n' '@' | grep -o 'PROG.*CROSSREFS' | tr '@' '\n' | sed 's/^[ \t]*//; s/<[^>]*>//g; s/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g; /^\s*$/d; /CROSSREFS/d; /PROG/d' | sed  's#//.*##g; s#\\.*##g; s#--.*##g; s#/\*.*##g; s/#.*//g'
-		printf "\n"
-	# Search unknown sequence
-	else
-		# Build URL
-		URL+="/search?q=$(echo $@ | tr ' ' ',')"
-		# Retrieve DOC
-		curl $URL 2>/dev/null > $DOC
-
-		grep -o '=id:.*&' $DOC | sed 's/=id://; s/&//' > $TMP/id
-		grep -A 1 '<td valign=top align=left>' $DOC | sed '/<td valign=top align=left>/d; /--/d; s/^[ \t]*//; s/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g; s/<[^>]*>//g' > $TMP/nam
-		grep -o '<tt>.*<b.*</tt>' $DOC | sed 's/<[^>]*>//g' > $TMP/seq
-
-		# Print data
-		readarray -t ID < $TMP/id
-		readarray -t NAM < $TMP/nam
-		readarray -t SEQ < $TMP/seq
-		for i in ${!NAM[@]}
-		do
-			printf "${ID[$i]}: ${NAM[$i]}\n"
-			echo ${SEQ[$i]}
-			echo "--"
-		done
-	fi
+  local URL='https://oeis.org'
+  local TMP=/tmp/oeis
+  local DOC=/tmp/oeis/doc.html
+  mkdir -p $TMP
+  # Search sequence by ID
+  if [ $# -eq 1 ]
+  then
+    # Generate URL
+    [[ ${1:0:1} == 'A' ]] && ID=${1:1} || ID=${1}
+    ID=$(bc <<< "$ID")
+    ID="A$(printf '%06d' ${ID})"
+    URL+="/${ID}"
+    curl $URL 2>/dev/null > $DOC
+    # ID
+    printf "ID: ${ID}\n"
+    # Description
+    grep -A 1 '<td valign=top align=left>' $DOC \
+      | sed '/<td valign=top align=left>/d; /--/d; s/^[ \t]*//; s/<[^>]*>//g;'
+    printf "\n"
+    # Sequence
+    grep -o '<tt>.*, .*[0-9]</tt>' $DOC \
+      | sed 's/<[^>]*>//g' \
+      | grep -v '[a-z]'
+    printf "\n"
+    # Code
+    cat $DOC \
+      | tr '\n' '@' \
+      | grep -o 'PROG.*CROSSREFS' \
+      | tr '@' '\n' \
+      | sed 's/^[ \t]*//; s/<[^>]*>//g' \
+      | sed 's/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g' \
+      | sed '/^\s*$/d; /CROSSREFS/d; /PROG/d' \
+      | sed  's#//.*##g; s#\\.*##g; s#--.*##g; s#/\*.*##g; s/#.*//g'
+    printf "\n"
+  # Search unknown sequence
+  else
+    # Build URL
+    URL+="/search?q=$(echo $@ | tr ' ' ',')"
+    curl $URL 2>/dev/null > $DOC
+    # Sequence IDs
+    grep -o '=id:.*&' $DOC \
+      | sed 's/=id://; s/&//' > $TMP/id
+    # Descriptions
+    grep -A 1 '<td valign=top align=left>' $DOC \
+      | sed '/<td valign=top align=left>/d; /--/d; s/^[ \t]*//; s/<[^>]*>//g' \
+      | sed 's/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g' > $TMP/nam
+    # Sequences
+    grep -o '<tt>.*<b.*</tt>' $DOC \
+      | sed 's/<[^>]*>//g' > $TMP/seq
+    # Print data for all
+    readarray -t ID < $TMP/id
+    readarray -t NAM < $TMP/nam
+    readarray -t SEQ < $TMP/seq
+    for i in ${!ID[@]}
+    do
+      printf "${ID[$i]}: ${NAM[$i]}\n"
+      echo ${SEQ[$i]}
+      printf "\n"
+    done
+  fi
 }
 
 oeis $@
