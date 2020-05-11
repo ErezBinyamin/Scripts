a33e56b89a6df9643aa5f4346095aa56f6867806
Your Name <you@example.com>
2020-05-10 23:19:50 -0400

    helper functions, seq sample max terms, language argument

--- tools/oeis.sh
+++ tools/oeis.sh
@@ -3,16 +3,43 @@
 # Written by Erez Binyamin (github.com/ErezBinyamin)
 
 # Search for an integer sequence
-oeis() {
+# USAGE:
+#	oeis <language> <sequence ID>
+#	oeis <sequence ID> <language>
+#	oeis <val_a, val_b, val_c, ...>
+oeis() (
   local URL='https://oeis.org'
   local TMP=/tmp/oeis
   local DOC=/tmp/oeis/doc.html
+  local MAX_TERMS=10
   mkdir -p $TMP
+  # Get short description of a sequence
+  get_desc() {
+    grep -A 1 '<td valign=top align=left>' $DOC \
+      | sed '/<td valign=top align=left>/d; /--/d; s/^[ \t]*//; s/<[^>]*>//g;' \
+      | sed 's/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g'
+  }
+  # Print out the first MAX_TERMS terms of a sequence
+  get_seq() {
+    grep -o '<tt>.*, .*[0-9]</tt>' $DOC \
+      | sed 's/<[^>]*>//g' \
+      | grep -v '[a-z]' \
+      | grep -v ':' \
+      | cut -d ',' -f 1-${MAX_TERMS}
+  }
   # Search sequence by ID
-  if [ $# -eq 1 ]
+  if [ $# -lt 3 ]
   then
-    # Generate URL
-    [[ ${1:0:1} == 'A' ]] && ID=${1:1} || ID=${1}
+    # Arg-Parse ID, Generate URL
+    if echo $1 | grep -q -e [a-z] -e [B-Z]
+    then
+      ID=$2
+      LANG=$1
+    else
+      ID=$1
+      LANG=$2
+    fi
+    [[ ${ID:0:1} == 'A' ]] && ID=${ID:1}
     ID=$(bc <<< "$ID")
     ID="A$(printf '%06d' ${ID})"
     URL+="/${ID}"
@@ -20,18 +47,13 @@ oeis() {
     # ID
     printf "ID: ${ID}\n"
     # Description
-    grep -A 1 '<td valign=top align=left>' $DOC \
-      | sed '/<td valign=top align=left>/d; /--/d; s/^[ \t]*//; s/<[^>]*>//g;' \
-      | sed 's/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g'
+    get_desc
     printf "\n"
     # Sequence
-    grep -o '<tt>.*, .*[0-9]</tt>' $DOC \
-      | sed 's/<[^>]*>//g' \
-      | grep -v '[a-z]' \
-      | grep -v ':'
+    get_seq
     printf "\n"
-    # Code
-    if grep -q 'MAPLE' $DOC
+    # Code Sample
+    if [[ ${LANG^^} == 'MAPLE' ]] && grep -q 'MAPLE' $DOC
     then
         GREP_REGEX='MAPLE.*CROSSREFS'
         grep -q 'PROG' $DOC && GREP_REGEX='MAPLE.*PROG'
@@ -43,9 +65,8 @@ oeis() {
           | sed 's/^[ \t]*//; s/<[^>]*>//g; /^\s*$/d;' \
           | sed 's/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g' \
           | sed 's/MAPLE/(MAPLE)/; /MATHEMATICA/d; /PROG/d; /CROSSREFS/d'
-        printf "\n"
     fi
-    if grep -q 'MATHEMATICA' $DOC
+    if [[ ${LANG^^} == 'MATHEMATICA' ]] && grep -q 'MATHEMATICA' $DOC
     then
         GREP_REGEX='MATHEMATICA.*CROSSREFS'
         grep -q 'PROG' $DOC && GREP_REGEX='MATHEMATICA.*PROG'
@@ -56,7 +77,6 @@ oeis() {
           | sed 's/^[ \t]*//; s/<[^>]*>//g; /^\s*$/d;' \
           | sed 's/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g' \
           | sed 's/MATHEMATICA/(MATHEMATICA)/; /PROG/d; /CROSSREFS/d'
-        printf "\n"
     fi
     # PROG section language support
     cat $DOC \
@@ -66,30 +86,24 @@ oeis() {
       | sed 's/^[ \t]*//; s/<[^>]*>//g; /^\s*$/d;' \
       | sed 's/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g' \
       | sed '/PROG/d; /CROSSREFS/d' > ${TMP}/lang
-    langs=("Axiom" "MAGMA" "PARI" "Python" "Sage" "Haskell" "Julia" "GAP" "Scala")
-    for L in ${langs[@]}
-    do
-        if grep -q "(${L})" $DOC
-        then
-              awk -v tgt="${L}" -F'[()]' '/^\(/{f=(tgt==$2)} f' ${TMP}/lang
-              printf "\n"
-        fi
-    done
+    # Print out code sample for specified language
+    awk -v tgt="${LANG^^}" -F'[()]' '/^\(/{f=(tgt==$2)} f' ${TMP}/lang
+    L="${LANG:0:1}"
+    LANG="${LANG:1}"
+    LANG="${L^^}${LANG,,}"
+    awk -v tgt="${LANG}" -F'[()]' '/^\(/{f=(tgt==$2)} f' ${TMP}/lang
   # Search unknown sequence
   else
     # Build URL
-    URL+="/search?q=signed%3A$(echo $@ | tr ' ' ',')"
+    URL+="/search?q=signed%3A$(echo $@ | grep -v [a-z] | grep -v [A-Z] | tr ' ' ',')"
     curl $URL 2>/dev/null > $DOC
     # Sequence IDs
     grep -o '=id:.*&' $DOC \
       | sed 's/=id://; s/&//' > $TMP/id
     # Descriptions
-    grep -A 1 '<td valign=top align=left>' $DOC \
-      | sed '/<td valign=top align=left>/d; /--/d; s/^[ \t]*//; s/<[^>]*>//g' \
-      | sed 's/&nbsp;/ /g; s/\&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g' > $TMP/nam
+    get_desc > $TMP/nam
     # Sequences
-    grep -o '<tt>.*<b.*</tt>' $DOC \
-      | sed 's/<[^>]*>//g' > $TMP/seq
+    get_seq > $TMP/seq
     # Print data for all
     readarray -t ID < $TMP/id
     readarray -t NAM < $TMP/nam
@@ -101,6 +115,6 @@ oeis() {
       printf "\n"
     done
   fi
-}
+)
 
 oeis $@
