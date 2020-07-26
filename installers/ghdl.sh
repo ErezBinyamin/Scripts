8608f8c6eca1a925f42ae37061127bec822afdfb
Erez Binyamin <exb4096@rit.edu>
2020-07-26 12:04:51 -0400

    Added ghdl installer

---
+++ installers/ghdl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y \
+  git \
+  make \
+  gnat \
+  zlib1g-dev
+
+git clone https://github.com/ghdl/ghdl
+cd ghdl
+./configure --prefix=/usr/local
+make
+sudo make install
+echo "$0: All done!"
+
+sudo apt install gtkwave
