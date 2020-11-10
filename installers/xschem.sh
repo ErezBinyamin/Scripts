909ce727b123ad7cadf944178bd823cfbe3c51b7
Erez Binyamin <exb4096@rit.edu>
2020-11-10 08:36:11 -0500

    xschem uninstall and functions

--- installers/xschem.sh
+++ installers/xschem.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 # http://repo.hu/projects/xschem/xschem_man/tutorial_install_xschem.html
 export PS4='\033[0;33m$0:$LINENO [$?]+ \033[0m '
-set -x
 
-# Pre-requisites
-sudo apt install subversion \
+xschem_uninstall() {
+	set -x
+	sudo rm -rf /usr/local/share/xschem/ /usr/local/share/doc/xschem/
+	rm -f  ~/xschemrc ~/.xschem/xschemrc /usr/local/bin/xschem
+	rm -rf /tmp/xschem_install
+	set +x
+}
+
+xschem_requirements() {
+	set -x
+	sudo apt install subversion \
                  tcl8.6 \
                  libx11-dev \
                  tk8.6 \
@@ -15,23 +23,46 @@ sudo apt install subversion \
                  flex \
                  gawk \
                  libcairo2-dev
+	set +x
+}
 
+xschem_install() {
+	set -x
+	mkdir -p /tmp/xschem_install
+	cd /tmp/xschem_install
+	mkdir build
+	cd build
+	svn checkout svn://repo.hu/xschem/trunk
 
-sudo rm -rf /usr/local/share/xschem/ /usr/local/share/doc/xschem/
-rm -f  ~/xschemrc ~/.xschem/xschemrc
-rm -rf /tmp/xschem_install
+	cd trunk
+	./configure --prefix=/usr/local --user-conf-dir=~/.xschem \
+	  --user-lib-path=~/share/xschem/xschem_library \
+	  --sys-lib-path=/usr/local/share/xschem/xschem_library
+	make
+	sudo make install
 
-mkdir -p /tmp/xschem_install
-cd /tmp/xschem_install
-mkdir build
-cd build
-svn checkout svn://repo.hu/xschem/trunk
+	cp src/xschemrc ~/.xschem
+	set +x
+}
 
-cd trunk
-./configure --prefix=/usr/local --user-conf-dir=~/.xschem \
-  --user-lib-path=~/share/xschem/xschem_library \
-  --sys-lib-path=/usr/local/share/xschem/xschem_library
-make
-sudo make install
+# If already installed
+if which xschem &>/dev/null
+then
+	while [[ ${C^^} != "Y" && ${C^^} != "N" ]]
+	do
+		read -p "xschem already installed. Uninstall xschem? [Y|N]" -sn1 C
+	done
+	[[ ${C^^} == "Y" ]] && xschem_uninstall
+	unset C
+fi
 
-cp src/xschemrc ~/.xschem
+while [[ ${C^^} != "Y" && ${C^^} != "N" ]]
+do
+	read -p "Install xschem? [Y|N]" -sn1 C
+done
+if [[ ${C^^} == "Y" ]]
+then
+	unset C
+	xschem_requirements
+	xschem_install
+fi
