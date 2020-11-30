f10b73026bc81961ab70b450d21aabf27342abf6
Erez Binyamin <exb4096@rit.edu>
2020-11-29 21:32:00 -0500

    ghdl functionified

--- installers/ghdl.sh
+++ installers/ghdl.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 export PS4='\033[0;33m$0:$LINENO [$?]+ \033[0m '
-set -x
 
-sudo apt update
-sudo apt install -y \
-  git \
-  make \
-  gnat \
-  zlib1g-dev
+choice() {
+  while true; do
+    read -p "Do you wish to install this program? " yn
+    case $yn in
+            [Yy]* ) make install; break;;
+            [Nn]* ) exit;;
+                * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
 
-git clone https://github.com/ghdl/ghdl
-cd ghdl
-./configure --prefix=/usr/local
-make
-sudo make install
-echo "$0: All done!"
+ghdl_req() {
+  set -x
+  sudo apt update
+  sudo apt install -y \
+    git \
+    make \
+    gnat \
+    zlib1g-dev \
+    gtkwave
+  set +x
+}
 
-sudo apt install gtkwave
+ghdl_install() {
+  set -x
+  cd /tmp
+  git clone https://github.com/ghdl/ghdl
+  cd ghdl
+  ./configure --prefix=/usr/local
+  make
+  sudo make install
+  echo "$0: All done!"
+  set +x
+}
+
+choice
+ghdl_req
+ghdl_install
