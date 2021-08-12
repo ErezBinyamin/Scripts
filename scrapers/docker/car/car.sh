26d108372fb2a117fe9cf5a821b04497b4c84d09
Erez Binyamin <ezbn2532@gmail.com>
2021-08-12 06:57:11 -0400

    added todo note in car.sh

--- scrapers/docker/car/car.sh
+++ scrapers/docker/car/car.sh
@@ -2,7 +2,10 @@
 
 car() {
 	local IMAGE_NAME=python/car
+
+	#TODO: Only rebuild when needed (use timestamps/Makefile)
 	docker build -t ${IMAGE_NAME} .
+
 	docker run -it ${IMAGE_NAME} python car.py $@
 }
 
