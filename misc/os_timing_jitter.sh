0703bc155f05d7280ee61bae78786d62b4ec7f71
Erez Binyamin <ezbn2532@gmail.com>
2021-10-03 11:18:16 -0400

    os_timing_jitter: parameterized packet size and added fifo step (to sub serial)

--- misc/os_timing_jitter.sh
+++ misc/os_timing_jitter.sh
@@ -8,19 +8,39 @@ delta_t() {
 }
 
 local_test() {
+	# Argparse
 	[ $# -ne 1 ] && return 1
+	[ $1 -gt 1 ] || return 1
 	local NUM_SAMPLES=${1}
-	local TEST_OUT_FILE=/tmp/local_test.csv
+	local PACKET_SIZE=${2:-"5"}
+	local TMP=/tmp/local_test_jitter
 
-	rm -f ${TEST_OUT_FILE}
+	# Packet will have new line apended
+	let PACKET_SIZE--
+	local PACKET=$(head -c ${PACKET_SIZE} < /dev/zero | tr '\0' '\141')
+
+	# Create output files and fifo for timing consistency
+	mkdir -p ${TMP}
+	local LOG=$(mktemp ${TMP}/test.XXXXX.dat)
+	echo "test output: ${LOG}"
+	local TFIFO=$(mktemp --dry-run /tmp/tfifo.XXXXX)
+	mkfifo ${TFIFO}
+
 	local COUNT=0
 	while [ ${COUNT} -lt ${NUM_SAMPLES} ]
 	do
 		T1=$(date +'%T.%N')
+
+		printf "${PACKET}\n" >> ${TFIFO} &
+		read RX < ${TFIFO}
+
 		T2=$(date +'%T.%N')
-		delta_t ${T1} ${T2} >> ${TEST_OUT_FILE}
+		delta_t ${T1} ${T2} >> ${LOG}
 		let COUNT++
 	done
+
+	# Cleanup
+	rm -rf ${TFIFO}
 }
 
 local_test $@
