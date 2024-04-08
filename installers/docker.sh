5bf001ea83dcf822a488492733b6aac351e33eaa
Erez Binyamin <ezbin@pm.me>
2024-04-07 23:59:26 -0400

    docker.sh: Replaced with get-docker.sh

--- installers/docker.sh
+++ installers/docker.sh
@@ -8,63 +8,36 @@ install_docker() {
 	if which docker &>/dev/null
 	then
 		echo [COMPLETE] docker already installed!
-	elif echo "Kinetic Jammy Focal Bionic" | grep --ignore-case --quiet "$(lsb_release -cs)"
-	then
-		# Set up the repository
-		## 1. Update the apt package index and install packages to allow apt to use a repository over HTTPS:
-		sudo apt update
-		sudo apt-get install \
-			ca-certificates \
-			curl \
-			gnupg \
-			lsb-release
-		## 2. 
-		sudo mkdir -m 0755 -p /etc/apt/keyrings
-		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-		## 3. Use the following command to set up the repository:
-		echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-		# Grant read permission for the Docker public key file before updating the package index:
-		sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-		# Install Docker Engine
-		## 1. Update the apt package index:
-		sudo apt-get update
-		## 2. Install Docker Engine, containerd, and Docker Compose.
-		sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-		# Post install https://docs.docker.com/engine/install/linux-postinstall/
-		## 1. Create the docker group.
-		sudo groupadd docker
-		## 2. Add your user to the docker group.
-		sudo usermod -aG docker $USER
-		## 3. run the following command to activate the changes to groups:
-		newgrp docker
-		## 4. change its ownership and permissions using the following commands:
-		sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
-		sudo chmod g+rwx "$HOME/.docker" -R
-		# Configure Docker to start on boot with systemd https://docs.docker.com/engine/install/linux-postinstall/#configure-docker-to-start-on-boot-with-systemd
-		sudo systemctl enable docker.service
-		sudo systemctl enable containerd.service
-		echo [COMPLETE] docker installed!
 	else
-		curl -fsSL https://get.docker.com -o get-docker.sh
-		sudo sh get-docker.sh
+		pushd "${PWD}"
+		cd /tmp
+
+		# 1. download the script
+		curl -fsSL https://get.docker.com -o install-docker.sh
+
+		# 2. verify the script's content
+		#cat install-docker.sh
+
+		# 3. run the script with --dry-run to verify the steps it executes
+		sh install-docker.sh --dry-run
+
+		# 4. run the script either as root, or using sudo to perform the installation.
+		sudo sh install-docker.sh
+
 		echo [COMPLETE] docker installed!
+		popd
 	fi
 
 	# Docker images
 	# DOCKER_IMAGES=()
+	# DOCKER_IMAGES+=("pihole/pihole")
+	# DOCKER_IMAGES+=("bettercap/bettercap")
 	# DOCKER_IMAGES+=("frolvlad/alpine-python2")
 	# DOCKER_IMAGES+=("frolvlad/alpine-python3")
 	# for dimage in ${DOCKER_IMAGES[$@]}
 	# do
 	# 	[[ "$(docker images -q ${dimage} 2> /dev/null)" == "" ]] && docker pull frolvlad/alpine-python2 || echo [COMPLETE] ${dimage}
 	# done
-
-
 }
 
 install_docker $@
